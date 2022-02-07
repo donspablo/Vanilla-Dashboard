7b3d14a1914e4511c61828adc477370aabe07f65
don Pablo <don@obeyi.com>
2022-02-07 04:34:48 +0200

    auto commit

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
-// ===================================================
-// Total.js start script
-// https://www.totaljs.com
-// ===================================================
+var options = {};
 
-const open = require('open');
-const http = require('http')
-const fs = require('fs')
+// ====================================
+// COMMON (OPTIONAL)
+// ====================================
 
-const options = {};
+// options.url = '/$dashboard/';
 
-var type = process.argv.indexOf('--release', 1) !== -1 || process.argv.indexOf('release', 1) !== -1 ? 'release' : 'debug';
-require('total4/' + type)(options);
+// A maximum length of request:
+// options.limit = 50;
 
+// Predefined set of components (default value):
+// options.templates = 'https://raw.githubusercontent.com/totaljs/dashboardcomponents/master/templates.json';
 
-exports.install = function () {
-    ROUTE('GET /index', 'index.html');
-};
+// ====================================
+// Security (OPTIONAL)
+// ====================================
 
+// +v6.0.0
+// Default light theme
+// options.dark = false;
 
-open('./index.html');
- 
+// +v6.0.0
+// Enables backing up of Dashboard designer
+// options.backup = true;
+// default: false
+
+// +v6.0.0
+// Enables link to Flow (if exists) in context menu
+// options.flow = true;
+// default: true
+
+// +v6.0.0
+// Enables link to Flowboard (if exists) in context menu
+// options.flowboard = true;
+// default: true
+
+// HTTP Basic auth:
+// options.auth = ['admin:admin', 'name:password'];
+
+// Standard "authorize" flag
+// options.auth = true;
+
+// IP restrictions:
+// options.restrictions = ['127.0.0.1', '138.201', '172.31.33'];
+
+// options.token = ['OUR_COMPANY_TOKEN'];
+// you can open dashboard using : /$dashboard/?token=OUR_COMPANY_TOKEN
+
+INSTALL('package', 'https://cdn.totaljs.com/dashboard.package', options);
